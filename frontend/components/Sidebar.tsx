7318401101194548a067a357ce398897dd8994f3
jakefrenzel <jakef1@hotmail.com>
2025-11-30 17:37:39 -0600

    CrownWynn v0.3.6 [Frontend - Fixed navigation links so that blank pages that don't exist aren't accessed but rather does nothing for now]

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -52,13 +52,13 @@ export default function Sidebar() {
           <div className={styles.section}>
             <h3 className={styles.section_title}>Support</h3>
             <div className={styles.support_links}>
-              <Link className={styles.support_link} href="/discord" onClick={closeMenu}>
+              <Link className={styles.support_link} href="#" onClick={closeMenu}>
                 <svg className={styles.link_icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M12 2C6.477 2 2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.879V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.989C18.343 21.129 22 16.99 22 12c0-5.523-4.477-10-10-10z"/>
                 </svg>
                 Discord
               </Link>
-              <Link className={styles.support_link} href="/terms" onClick={closeMenu}>
+              <Link className={styles.support_link} href="#" onClick={closeMenu}>
                 <svg className={styles.link_icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"/>
                 </svg>
